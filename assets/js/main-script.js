94a533c1cfc80cdf5c1a0267fc31ed7fe7d5149f
A.Habbe <73256497+habbussDev@users.noreply.github.com>
2023-10-30 22:15:33 +0100

    11. Mobile menu

---
+++ assets/js/main-script.js
@@ -0,0 +1,40 @@
+(function($) {
+"use strict";
+
+//mobile nav
+if($('.mobile-menu').length){
+
+    var mobileMenuContent = $('#top-navigation .navigation').html();
+
+    $('.mobile-menu .navigation').append(mobileMenuContent);
+
+    $('.sticky-header .navigation').append(mobileMenuContent);
+
+    $('.mobile-menu .close-btn').on('click', function() {
+        $('body').removeClass('mobile-menu-visible');
+    });
+
+    // Dropdown button
+
+    $(',mobile-menu li.menu-item-has-children .dropdown-btn').on('click',
+    function() {
+        $(this).prev('ul').slideToggle(500);
+    });
+
+    // menu toggle button
+
+    $('.mobile-nav-toggler').on('click', function() {
+        $('body').addClass('mobile-meu-visible');
+    });
+
+
+    // menu toggle button
+
+    $(".mobile-menu .menu-backdrop, .mobile-menu .close-btn").on(
+        "click",
+        function () {
+          $("body").removeClass("mobile-menu-visible");
+    });
+
+}
+})(jQuery);
